a89334944dad4df821082a94ee814c8efc9218a4
spd789562 <leo.yicun.lin@gmail.com>
2020-10-19 15:46:54 +0800

    [add] hair format helper and color select

--- src/util/group-hair.js
+++ src/util/group-hair.js
@@ -7,13 +7,24 @@ const colorRemover = (str) =>
     .trim()
 
 /**
+ * formatHairId
+ * @description format hair id to generic hair id
+ * @param {number|string} id - hari id
+ * @return {number}
+ * @example
+ *  formatHairId(30000) // -> 3000
+ *  formatHairId(34503) // -> 3450
+ */
+export const formatHairId = (id) => Math.floor(+id / 10)
+
+/**
  * groupHair
  * @description Group hair list
  * @param {Array<Face>} hairs
  */
 const groupHair = function groupHair(hair) {
   return hair.reduce((allHair, face) => {
-    const groupId = Math.floor(face.id / 10)
+    const groupId = formatHairId(face.id)
     const colorType = face.id % 10
     if (!allHair[groupId]) {
       const name = name
