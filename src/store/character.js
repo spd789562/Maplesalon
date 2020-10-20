9aecca8633a088bf30f0ddcf201712d08d062f36
spd789562 <leo.yicun.lin@gmail.com>
2020-10-20 11:39:37 +0800

    [edit] character list default value

--- src/store/character.js
+++ src/store/character.js
@@ -8,9 +8,56 @@ export const CHARACTER_UPDATE = 'CHARACTER_UPDATE'
 export const CHARACTER_DUPLICATE = 'CHARACTER_DUPLICATE'
 export const CHARACTER_DELETE = 'CHARACTER_DELETE'
 
+const isClient = typeof window !== 'undefined'
+const initialCharacters = (isClient &&
+  localStorage.getItem('MAPLESALON_characters')) || [
+  {
+    id: 100000000,
+    type: 'character',
+    action: 'stand1',
+    emotion: 'default',
+    skin: '2000',
+    frame: 0,
+    mercEars: false,
+    illiumEars: false,
+    highFloraEars: false,
+    selectedItems: {
+      Body: {
+        id: 2000,
+        region: 'TWMS',
+        version: '228',
+      },
+      Head: {
+        id: 12000,
+        region: 'TWMS',
+        version: '228',
+      },
+      Face: {
+        requiredGender: 1,
+        name: '새초롬 얼굴',
+        id: 28087,
+        region: 'KMS',
+        version: '338',
+      },
+      Hair: {
+        requiredGender: 2,
+        name: '紅色齊眉自然造型',
+        id: 30301,
+        region: 'TWMS',
+        version: '228',
+      },
+    },
+    visible: true,
+    fhSnap: true,
+    flipX: false,
+    name: '中文',
+    animating: false,
+  },
+]
+
 const initialState = {
-  characters: [],
-  currentId: 0,
+  characters: (isClient && localStorage.getItem('MAPLESALON_characters')) || [],
+  currentId: initialCharacters[0].id,
 }
 
 const reducer = reducerCreator(initialState, {
