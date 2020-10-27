d9d4167ec00d4515ddabd5addf5a231713ab36c7
spd789562 <leo.yicun.lin@gmail.com>
2020-10-27 16:32:20 +0800

    [edit] add padding

--- src/component/skin-tab/image/index.jsx
+++ src/component/skin-tab/image/index.jsx
@@ -36,7 +36,7 @@ const Image = ({ columnIndex, rowIndex, style, data }) => {
             display: flex;
             flex-direction: column;
             align-items: center;
-            justify-content: center;
+            padding-top: 10px;
             font-size: 12px;
           }
           .item__selected {
