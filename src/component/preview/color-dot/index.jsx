fa1fc545df2323d7e4f9b09fb9aa39617f17a558
spd789562 <leo.yicun.lin@gmail.com>
2020-10-29 16:57:51 +0800

    [add] color dot

---
+++ src/component/preview/color-dot/index.jsx
@@ -0,0 +1,18 @@
+import { memo } from 'react'
+
+const ColorDot = ({ color, name }) => (
+  <div className="color-dot" style={{ backgroundColor: color }} title={name}>
+    <style jsx>{`
+      .color-dot {
+        width: 28px;
+        height: 28px;
+        border-radius: 50%;
+        display: inline-block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    `}</style>
+  </div>
+)
+
+export default memo(ColorDot)
