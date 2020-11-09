8fc9189c6ec75b78ab3b6b84b7874de940388e96
spd789562 <leo.yicun.lin@gmail.com>
2020-11-09 11:24:15 +0800

    [add] differences add scale

---
+++ src/component/character-different/scale/index.jsx
@@ -0,0 +1,32 @@
+/* components */
+import { Radio } from 'antd'
+
+const ScaleOption = [
+  { label: '1x', value: 'origin' },
+  { label: '2x', value: 'origin-2' },
+  { label: 'Full', value: 'full' },
+]
+
+const Scale = ({ onChange }) => {
+  return (
+    <div className="different-scale">
+      <Radio.Group
+        options={ScaleOption}
+        optionType="button"
+        buttonStyle="solid"
+        style={{ width: '100%' }}
+        onChange={({ target: { value } }) => onChange(value)}
+        defaultValue={'full'}
+      />
+      <style jsx>{`
+        .different-scale {
+          margin-left: auto;
+          margin-right: auto;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Scale
