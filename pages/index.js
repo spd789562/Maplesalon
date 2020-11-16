f9507b4f1887d8d6d14a17ba506488923cf018a7
spd789562 <leo.yicun.lin@gmail.com>
2020-11-16 11:01:44 +0800

    [add] move apparance tab component to individual component

--- pages/index.js
+++ pages/index.js
@@ -1,121 +1,42 @@
-import React, { useState, useCallback, useEffect, useMemo, memo } from 'react'
-import dynamic from 'next/dynamic'
-import { Row, Col, Card, Select } from 'antd'
-
-import { APIGetWz } from '@api'
-import { useStore } from '@store'
-import { INITIAL_WZ, CHANGE_REGION } from '@store/meta'
+import React, { memo } from 'react'
+import { Row, Col, Card } from 'antd'
 
 /* component */
-import HairTab from '@components/apparance-tabs/hair-tab'
-import FaceTab from '@components/apparance-tabs/face-tab'
-import SkinTab from '@components/apparance-tabs/skin-tab'
-import EarsTab from '@components/apparance-tabs/ears-tab'
-import MixDyeHairTab from '@components/apparance-tabs/mix-dye-hair-tab'
-import MixDyeFaceTab from '@components/apparance-tabs/mix-dye-face-tab'
-import HatTab from '@components/apparance-tabs/hat-tab'
-import OverallTab from '@components/apparance-tabs/overall-tab'
+import ApparanceTabs from '@components/apparance-tabs'
 import Preview from '@components/preview'
 import MixPreview from '@components/preview/mix-preview'
-import TabResizer from '@components/tab-resizer'
 import CharacterDifferent from '@components/character-different'
 import CharacterList from '@components/character-list'
 import RandomStyle from '@components/random-style'
 
-/* mapping */
-import LanguageToRegion from '@mapping/language-to-region'
-
 /* helper */
-import { withTranslation } from '../src/i18n'
-import { assoc, evolve } from 'ramda'
+import { withTranslation } from '@i18n'
 
-const ApparanceTabs = [
-  { key: 'hair', tab: 'tab_hair', Component: <HairTab /> },
-  { key: 'face', tab: 'tab_face', Component: <FaceTab /> },
-  { key: 'skin', tab: 'tab_skin', Component: <SkinTab /> },
-  { key: 'ears', tab: 'tab_ears', Component: <EarsTab /> },
-  { key: 'mixdyehair', tab: 'tab_mix_dye_hair', Component: <MixDyeHairTab /> },
-  { key: 'mixdyeface', tab: 'tab_mix_dye_face', Component: <MixDyeFaceTab /> },
-  { key: 'hat', tab: 'tab_hat', Component: <HatTab /> },
-  { key: 'overall', tab: 'tab_overall', Component: <OverallTab /> },
-]
-
-const TabMapping = ApparanceTabs.reduce(
-  (mapping, tab) => assoc(tab.key, tab, mapping),
-  {}
-)
-
-const useInitWz = (language) => {
-  const [region, dispatch] = useStore('meta.region.region', '')
-  useEffect(() => {
-    APIGetWz().then((data) => {
-      dispatch({ type: INITIAL_WZ, payload: data })
-      if (!region)
-        dispatch({
-          type: CHANGE_REGION,
-          payload: data[LanguageToRegion[language] || 'GMS'],
-        })
-    })
-  }, [])
-  const handleChangeWz = (value) =>
-    dispatch({
-      type: CHANGE_REGION,
-      payload: value,
-    })
-  return { region, handleChangeWz }
-}
-
-function Home({ t, i18n }) {
-  const { region, handleChangeWz } = useInitWz(i18n.language)
-  const [tab, changeTab] = useState(ApparanceTabs[0].key)
-  const translateTab = useMemo(() => ApparanceTabs.map(evolve({ tab: t })), [
-    i18n.language,
-  ])
-  // const TabComponent = useMemo(() => TabMapping[tab].Component, [tab])
-  return useMemo(
-    () => (
-      <Row gutter={[16, 16]}>
-        <Col span={24} lg={12} xl={8}>
-          <Card
-            title={t('character_apparance')}
-            bordered={false}
-            tabList={translateTab}
-            onTabChange={changeTab}
-            extra={
-              <Select value={region} onChange={handleChangeWz} name="region">
-                <Select.Option value="KMS">KMS</Select.Option>
-                <Select.Option value="GMS">GMS</Select.Option>
-                <Select.Option value="TWMS">TWMS</Select.Option>
-                <Select.Option value="JMS">JMS</Select.Option>
-                <Select.Option value="CMS">CMS</Select.Option>
-              </Select>
-            }
-          >
-            <TabResizer />
-            {TabMapping[tab].Component}
-          </Card>
-        </Col>
-        <Col span={24} lg={12} xl={16}>
-          <Card
-            title={t('character_comparison')}
-            bordered={false}
-            extra={<RandomStyle />}
-          >
-            <CharacterDifferent />
-          </Card>
-          <Card title={t('character_list')} bordered={false}>
-            <CharacterList />
-          </Card>
-        </Col>
-        <Col span={24}>
-          <Preview />
-        </Col>
-        <Col span={24}>
-          <MixPreview />
-        </Col>
-      </Row>
-    ),
-    [i18n.language, region, tab]
+function Home({ t }) {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={24} lg={12} xl={8}>
+        <ApparanceTabs />
+      </Col>
+      <Col span={24} lg={12} xl={16}>
+        <Card
+          title={t('character_comparison')}
+          bordered={false}
+          extra={<RandomStyle />}
+        >
+          <CharacterDifferent />
+        </Card>
+        <Card title={t('character_list')} bordered={false}>
+          <CharacterList />
+        </Card>
+      </Col>
+      <Col span={24}>
+        <Preview />
+      </Col>
+      <Col span={24}>
+        <MixPreview />
+      </Col>
+    </Row>
   )
 }
 
---
+++ src/component/apparance-tabs/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useCallback, useEffect, useMemo, memo } from 'react'
+import { Card, Select } from 'antd'
+
+import { APIGetWz } from '@api'
+import { useStore } from '@store'
+import { INITIAL_WZ, CHANGE_REGION } from '@store/meta'
+
+/* component */
+import HairTab from './hair-tab'
+import FaceTab from './face-tab'
+import SkinTab from './skin-tab'
+import EarsTab from './ears-tab'
+import MixDyeHairTab from './mix-dye-hair-tab'
+import MixDyeFaceTab from './mix-dye-face-tab'
+import HatTab from './hat-tab'
+import OverallTab from './overall-tab'
+import TabResizer from './tab-resizer'
+
+/* mapping */
+import LanguageToRegion from '@mapping/language-to-region'
+
+/* helper */
+import { withTranslation } from '@i18n'
+import { assoc, evolve } from 'ramda'
+
+const ApparanceTabMapping = [
+  { key: 'hair', tab: 'tab_hair', Component: <HairTab /> },
+  { key: 'face', tab: 'tab_face', Component: <FaceTab /> },
+  { key: 'skin', tab: 'tab_skin', Component: <SkinTab /> },
+  { key: 'ears', tab: 'tab_ears', Component: <EarsTab /> },
+  { key: 'mixdyehair', tab: 'tab_mix_dye_hair', Component: <MixDyeHairTab /> },
+  { key: 'mixdyeface', tab: 'tab_mix_dye_face', Component: <MixDyeFaceTab /> },
+  { key: 'hat', tab: 'tab_hat', Component: <HatTab /> },
+  { key: 'overall', tab: 'tab_overall', Component: <OverallTab /> },
+]
+
+const TabMapping = ApparanceTabMapping.reduce(
+  (mapping, tab) => assoc(tab.key, tab, mapping),
+  {}
+)
+
+const useInitWz = (language) => {
+  const [region, dispatch] = useStore('meta.region.region', '')
+  useEffect(() => {
+    APIGetWz().then((data) => {
+      dispatch({ type: INITIAL_WZ, payload: data })
+      if (!region)
+        dispatch({
+          type: CHANGE_REGION,
+          payload: data[LanguageToRegion[language] || 'GMS'],
+        })
+    })
+  }, [])
+  const handleChangeWz = (value) =>
+    dispatch({
+      type: CHANGE_REGION,
+      payload: value,
+    })
+  return { region, handleChangeWz }
+}
+
+function ApparanceTabs({ t, i18n }) {
+  const { region, handleChangeWz } = useInitWz(i18n.language)
+  const [tab, changeTab] = useState(ApparanceTabMapping[0].key)
+  const translateTab = useMemo(
+    () => ApparanceTabMapping.map(evolve({ tab: t })),
+    [i18n.language]
+  )
+  return useMemo(
+    () => (
+      <Card
+        title={t('character_apparance')}
+        bordered={false}
+        tabList={translateTab}
+        onTabChange={changeTab}
+        extra={
+          <Select value={region} onChange={handleChangeWz} name="region">
+            <Select.Option value="KMS">KMS</Select.Option>
+            <Select.Option value="GMS">GMS</Select.Option>
+            <Select.Option value="TWMS">TWMS</Select.Option>
+            <Select.Option value="JMS">JMS</Select.Option>
+            <Select.Option value="CMS">CMS</Select.Option>
+          </Select>
+        }
+      >
+        <TabResizer />
+        {TabMapping[tab].Component}
+      </Card>
+    ),
+    [region, tab]
+  )
+}
+
+ApparanceTabs.getInitialProps = async () => ({
+  namespacesRequired: ['index'],
+})
+
+export default withTranslation('index')(memo(ApparanceTabs))
